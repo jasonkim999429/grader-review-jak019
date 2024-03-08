f8fab1243c1e20ac28b032e4127a6be96181120b
Carl Ingelsson <cingelsson@ieng6-201.ucsd.edu>
2024-03-07 17:27:44 -0800

    Removed uncessesary comments

--- grade.sh
+++ grade.sh
@@ -8,13 +8,6 @@ mkdir grading-area
 git clone $1 student-submission
 echo 'Finished cloning'
 
-
-# Draw a picture/take notes on the directory structure that's set up after
-# getting to this point
-
-# Then, add here code to compile and run, and do any post-processing of the
-# tests
-
 if [[ -f student-submission/ListExamples.java ]]
 then
     echo "File found"
